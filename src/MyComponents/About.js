75a4439b885733678f06fe83afd3142b70e0bbab
Jenny20819 <108798838+Jenny20819@users.noreply.github.com>
2022-08-29 19:28:46 +0530

    to-do list

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      <p> this is an about component</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dicta quia iusto nulla tenetur esse laboriosam corporis vel optio laborum ea architecto aliquid obcaecati blanditiis quas dignissimos cumque, laudantium suscipit asperiores vitae. Ratione, nemo?</p>
+    </div>
+  );
+};
